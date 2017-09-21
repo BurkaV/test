817c3056291b663c89f0d5034d35ec73b10cf698
BurkaV <spirit.jet@gmail.com>
2017-09-21 14:25:51 +0300

    3 file

---
+++ wice.py
@@ -0,0 +1,21 @@
+def do_twice(f):
+	print(f)
+	print(f)
+    
+do_twice('1')
+
+#def print_twice(arg):
+  #  print (arg)
+  #  print (arg)
+#print_twice("12345689")
+
+
+#do_twice(print_twice, 'spam')
+#print ''
+
+#def do_four(f, arg):
+ #   do_twice(f, arg)
+  #  do_twice(f, arg)
+
+#do_four(print_twice, 'spam')
+#print ''
