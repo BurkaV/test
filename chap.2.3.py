bcc4b5a4059ddaf30de7f7e0777f9bbba64c4b91
BurkaV <spirit.jet@gmail.com>
2017-09-21 14:45:24 +0300

    4 file

--- chap.2.3.py
+++ chap.2.3.py
@@ -2,8 +2,8 @@ width = 17
 height = 12.0
 delimiter = '.'
 
-print(width/2)
-print(width/2.0)
-print(height/3)
-print(1 + 2 * 5)
-print(delimiter * 5)
+print(width/2, type (width/2))
+print(width/2.0, type (width/2.0))
+print(height/3, type (height/3))
+print(1 + 2 * 5, type (1+2*5))
+print(delimiter * 5, type (delimiter *5))
